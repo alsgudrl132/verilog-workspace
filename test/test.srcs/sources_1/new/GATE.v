938148cb182015e8f94348a1ce816ce14032146b
alsgudrl132 <alsgudrl132@gmail.com>
2025-07-14 10:04:53 +0900

    first commit

---
+++ test/test.srcs/sources_1/new/GATE.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/14/2025 09:59:38 AM
+// Design Name: 
+// Module Name: GATE
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module GATE(
+
+    );
+endmodule
